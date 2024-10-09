887b55a646caaabeca117a664aadb3c2ee824c70
Noire.dev <141606820+noire-dev@users.noreply.github.com>
2024-10-09 14:31:03 -0700

    update bsp limits

--- code/qcommon/qfiles.h
+++ code/qcommon/qfiles.h
@@ -346,8 +346,8 @@ typedef struct {
 #define	LIGHTMAP_WIDTH		128
 #define	LIGHTMAP_HEIGHT		128
 
-#define MAX_WORLD_COORD		( 128*1024 )
-#define MIN_WORLD_COORD		( -128*1024 )
+#define MAX_WORLD_COORD		( 1048576 )
+#define MIN_WORLD_COORD		( -1048576 )
 #define WORLD_SIZE			( MAX_WORLD_COORD - MIN_WORLD_COORD )
 
 //=============================================================================
