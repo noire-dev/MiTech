898b75a8fff5e24033bb32fd3133552d768ab98d
Noire.dev <141606820+noire-dev@users.noreply.github.com>
2024-10-19 02:47:09 +0300

    upd

--- code/renderer2/tr_marks.c
+++ code/renderer2/tr_marks.c
@@ -165,8 +165,7 @@ static void R_BoxSurfaces_r(mnode_t *node, vec3_t mins, vec3_t maxs, surfaceType
 		// check if the surface has NOIMPACT or NOMARKS set
 		if ( ( surf->shader->surfaceFlags & ( SURF_NOIMPACT | SURF_NOMARKS ) )
 			|| ( surf->shader->contentFlags & CONTENTS_FOG ) ) {
-			//*surfViewCount = tr.viewCount;
-			continue;
+			*surfViewCount = tr.viewCount;
 		}
 		// extra check for surfaces to avoid list overflows
 		else if (*(surf->data) == SF_FACE) {
