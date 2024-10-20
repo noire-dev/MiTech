3a8568f60342ab1520568ad0f47c41d469b5da03
Noire.dev <141606820+noire-dev@users.noreply.github.com>
2024-10-20 12:41:24 +0300

    UPDATE

--- code/renderervk/tr_light.c
+++ code/renderervk/tr_light.c
@@ -325,7 +325,7 @@ void R_SetupEntityLighting( const trRefdef_t *refdef, trRefEntity_t *ent ) {
 	}
 
 	// bonus items and view weapons have a fixed minimum add
-	if ( 1 /* ent->e.renderfx & RF_MINLIGHT */ ) {
+	if ( ent->e.renderfx & RF_MINLIGHT ) {
 		// give everything a minimum light add
 		ent->ambientLight[0] += tr.identityLight * 32;
 		ent->ambientLight[1] += tr.identityLight * 32;
